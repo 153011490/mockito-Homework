a5405b674ef132890cd58e6b4645eaf9c4a400ee
SamsonLin <153011490@qq.com>
2019-08-11 15:05:28 +0800

    refactor:refactor function generateSalesActivityReport in Class SalesApp

---
+++ src/main/java/sales/SalesApp.java
@@ -0,0 +1,69 @@
+package sales;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class SalesApp {
+
+	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
+
+		if (salesId == null) {
+			return;
+		}
+
+		Sales sales = getSalesById(salesId);
+
+		if (isSalesEffective(sales)) return;
+
+		List<SalesReportData> reportDataList = getSalesReportData(sales);
+
+		List<String> headers = getHeaders(isNatTrade);
+
+		SalesActivityReport report = this.generateReport(headers, reportDataList);
+
+		uploadDocument(report);
+
+	}
+
+	private void uploadDocument(SalesActivityReport report) {
+		EcmService ecmService = new EcmService();
+		ecmService.uploadDocument(report.toXml());
+	}
+
+	private List<String> getHeaders(boolean isNatTrade) {
+		List<String> headers = null;
+		if (isNatTrade) {
+			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
+		} else {
+			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
+		}
+		return headers;
+	}
+
+	private boolean isSalesEffective(Sales sales) {
+		Date today = new Date();
+		if (today.after(sales.getEffectiveTo())
+				|| today.before(sales.getEffectiveFrom())){
+			return true;
+		}
+		return false;
+	}
+
+	private List<SalesReportData> getSalesReportData(Sales sales) {
+		SalesReportDao salesReportDao = new SalesReportDao();
+		return salesReportDao.getReportData(sales);
+	}
+
+	private Sales getSalesById(String salesId) {
+		SalesDao salesDao = new SalesDao();
+		return salesDao.getSalesBySalesId(salesId);
+	}
+
+	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
